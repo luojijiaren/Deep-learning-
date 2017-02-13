26e7494a308e268bcceb52178be065442f7fb564
Fangling Zhang <luojijiaren@163.com>
2017-02-13 15:30:52 -0500

    HW3

---
+++ HW3/homework3_FanglingZhang.py
@@ -0,0 +1,213 @@
+# Problem 2
+import numpy as np
+
+def J (w, faces, labels, alpha = 0.):
+    y_hat=np.dot(faces,w)
+    dif=y_hat-labels
+    J=1/2*np.dot(dif.T,dif)+alpha/2*np.dot(w.T,w)
+    return J  
+
+def gradJ (w, faces, labels, alpha = 0.):
+    dif=np.dot(faces,w)-labels
+    gradJ=np.dot(faces.T,dif)+alpha*w
+    return gradJ 
+
+def gradientDescent (trainingFaces, trainingLabels, testingFaces, testingLabels, alpha = 0.):
+    w = np.zeros(trainingFaces.shape[1])  # Or set to random vector
+    J_dif=0.1
+    Des_gradJ= np.zeros(trainingFaces.shape[1])     
+    while J_dif > 0.001:
+        J0=J (w, trainingFaces, trainingLabels,alpha)
+        Des_gradJ=gradJ (w, trainingFaces, trainingLabels,alpha)
+        w=w-0.0005*Des_gradJ
+        J1=J (w, trainingFaces, trainingLabels,alpha)
+        J_dif=J0-J1
+    return w
+
+
+def method2 (trainingFaces, trainingLabels, testingFaces, testingLabels):
+    return gradientDescent(trainingFaces, trainingLabels, testingFaces, testingLabels)
+
+def method3 (trainingFaces, trainingLabels, testingFaces, testingLabels):
+    alpha = 0.08
+    return gradientDescent(trainingFaces, trainingLabels, testingFaces, testingLabels, alpha)
+
+def reportCosts (w, trainingFaces, trainingLabels, testingFaces, testingLabels, alpha = 0.):
+    print ("Training cost: {}".format(J(w, trainingFaces, trainingLabels, alpha)))
+    print ("Testing cost:  {}".format(J(w, testingFaces, testingLabels, alpha)))
+
+# Accesses the web camera, displays a window showing the face, and classifies smiles in real time
+# Requires OpenCV.
+def detectSmiles (w):
+    # Given the image captured from the web camera, classify the smile
+    def classifySmile (im, imGray, faceBox, w):
+        # Extract face patch as vector
+        face = imGray[faceBox[1]:faceBox[1]+faceBox[3], faceBox[0]:faceBox[0]+faceBox[2]]
+        face = cv2.resize(face, (24, 24))
+        face = (face - np.mean(face)) / np.std(face)  # Normalize
+        face = np.reshape(face, face.shape[0]*face.shape[1])
+
+        # Classify face patch
+        yhat = w.dot(face)
+        print (yhat)
+
+        # Draw result as colored rectangle
+        THICKNESS = 3
+        green = 128 + (yhat - 0.5) * 255
+        color = (0, green, 255 - green)
+        pt1 = (faceBox[0], faceBox[1])
+        pt2 = (faceBox[0]+faceBox[2], faceBox[1]+faceBox[3])
+        cv2.rectangle(im, pt1, pt2, color, THICKNESS)
+
+    # Starting video capture
+    vc = cv2.VideoCapture()
+    vc.open(0)
+    faceDetector = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")  # TODO update the path
+    while vc.grab():
+        (tf,im) = vc.read()
+        im = cv2.resize(im, (im.shape[1]/2, im.shape[0]/2))  # Divide resolution by 2 for speed
+        imGray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        k = cv2.waitKey(30)
+        if k >= 0 and chr(k) == 'q':
+            print ("quitting")
+            break
+
+        # Detect faces
+        faceBoxes = faceDetector.detectMultiScale(imGray)
+        for faceBox in faceBoxes:
+            classifySmile(im, imGray, faceBox, w)
+        cv2.imshow("WebCam", im)
+
+    cv2.destroyWindow("WebCam")
+    vc.release()
+
+if __name__ == "__main__":
+    # Load data
+    if ('trainingFaces' not in globals()):  # In ipython, use "run -i homework2_template.py" to avoid re-loading of data
+        trainingFaces = np.load("trainingFaces.npy")
+        trainingLabels = np.load("trainingLabels.npy")
+        testingFaces = np.load("testingFaces.npy")
+        testingLabels = np.load("testingLabels.npy")
+
+        A1=np.dot(trainingFaces.T,trainingFaces)
+        A=A1+0.000001*np.identity(576)
+        eval,evec = np.linalg.eigh(A)
+        eval2=np.power(eval,-1/2)
+        fai=np.diag(eval2)
+        L=np.dot(evec,fai)
+        md_trainingFaces=np.dot(trainingFaces,L)
+        md_testingFaces=np.dot(testingFaces,L)
+        
+    w2 = method2(md_trainingFaces, trainingLabels, md_testingFaces, testingLabels)
+    w3 = method3(md_trainingFaces, trainingLabels, md_testingFaces, testingLabels)
+
+    for w in [ w2, w3 ]:
+        reportCosts(w, md_trainingFaces, trainingLabels, md_testingFaces, testingLabels)
+        
+
+        
+        
+        
+        
+# Problem 3
+        
+        
+import numpy as np
+
+def J (w, faces, labels, alpha = 0.):
+    m=labels.size
+    z=np.dot(faces,w)
+    y_hat=np.reciprocal(1+np.exp(-z))
+    J=-1/m*(np.dot(labels.T,np.log(y_hat))+np.dot((1-labels).T,np.log(1-y_hat)))+alpha/2*np.dot(w.T,w)
+    return J  
+
+def gradJ (w, faces, labels, alpha = 0.):
+    m=labels.size
+    z=np.dot(faces,w)
+    y_hat=np.reciprocal(1+np.exp(-z))
+    dif=y_hat-labels
+    gradJ=1/m*np.dot(faces.T,dif)+alpha*w
+    return gradJ 
+
+def gradientDescent (trainingFaces, trainingLabels, testingFaces, testingLabels, alpha = 0.):
+    w = np.zeros(trainingFaces.shape[1])  # Or set to random vector
+    J_dif=0.1
+    Des_gradJ= np.zeros(trainingFaces.shape[1])     
+    while J_dif > 0.001:
+        J0=J (w, trainingFaces, trainingLabels,alpha)
+        Des_gradJ=gradJ (w, trainingFaces, trainingLabels,alpha)
+        w=w-0.05*Des_gradJ
+        J1=J (w, trainingFaces, trainingLabels,alpha)
+        J_dif=J0-J1
+    return w
+
+
+def method2 (trainingFaces, trainingLabels, testingFaces, testingLabels):
+    return gradientDescent(trainingFaces, trainingLabels, testingFaces, testingLabels)
+
+def method3 (trainingFaces, trainingLabels, testingFaces, testingLabels):
+    alpha = 1e-2
+    return gradientDescent(trainingFaces, trainingLabels, testingFaces, testingLabels, alpha)
+
+def reportCosts (w, trainingFaces, trainingLabels, testingFaces, testingLabels, alpha = 0.):
+    print ("Training cost: {}".format(J(w, trainingFaces, trainingLabels, alpha)))
+    print ("Testing cost:  {}".format(J(w, testingFaces, testingLabels, alpha)))
+
+# Accesses the web camera, displays a window showing the face, and classifies smiles in real time
+# Requires OpenCV.
+def detectSmiles (w):
+    # Given the image captured from the web camera, classify the smile
+    def classifySmile (im, imGray, faceBox, w):
+        # Extract face patch as vector
+        face = imGray[faceBox[1]:faceBox[1]+faceBox[3], faceBox[0]:faceBox[0]+faceBox[2]]
+        face = cv2.resize(face, (24, 24))
+        face = (face - np.mean(face)) / np.std(face)  # Normalize
+        face = np.reshape(face, face.shape[0]*face.shape[1])
+
+        # Classify face patch
+        yhat = w.dot(face)
+        print (yhat)
+
+        # Draw result as colored rectangle
+        THICKNESS = 3
+        green = 128 + (yhat - 0.5) * 255
+        color = (0, green, 255 - green)
+        pt1 = (faceBox[0], faceBox[1])
+        pt2 = (faceBox[0]+faceBox[2], faceBox[1]+faceBox[3])
+        cv2.rectangle(im, pt1, pt2, color, THICKNESS)
+
+    # Starting video capture
+    vc = cv2.VideoCapture()
+    vc.open(0)
+    faceDetector = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")  # TODO update the path
+    while vc.grab():
+        (tf,im) = vc.read()
+        im = cv2.resize(im, (im.shape[1]/2, im.shape[0]/2))  # Divide resolution by 2 for speed
+        imGray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        k = cv2.waitKey(30)
+        if k >= 0 and chr(k) == 'q':
+            print ("quitting")
+            break
+
+        # Detect faces
+        faceBoxes = faceDetector.detectMultiScale(imGray)
+        for faceBox in faceBoxes:
+            classifySmile(im, imGray, faceBox, w)
+        cv2.imshow("WebCam", im)
+
+    cv2.destroyWindow("WebCam")
+    vc.release()
+
+if __name__ == "__main__":
+    # Load data
+    if ('trainingFaces' not in globals()):  # In ipython, use "run -i homework2_template.py" to avoid re-loading of data
+        trainingFaces = np.load("trainingFaces.npy")
+        trainingLabels = np.load("trainingLabels.npy")
+        testingFaces = np.load("testingFaces.npy")
+        testingLabels = np.load("testingLabels.npy")
+
+    w2 = method2(trainingFaces, trainingLabels, testingFaces, testingLabels)
+    w3 = method3(trainingFaces, trainingLabels, testingFaces, testingLabels)
+
+    for w in [ w2, w3 ]:
+        reportCosts(w, trainingFaces, trainingLabels, testingFaces, testingLabels)
