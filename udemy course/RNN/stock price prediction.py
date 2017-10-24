b535eed68d62b9ca0cfaa30a25f42ceb13d82e56
Fangling Zhang <luojijiaren@163.com>
2017-10-24 16:25:26 -0400

    use rnn to predict stock price

--- udemy course/RNN/stock price prediction.py
+++ udemy course/RNN/stock price prediction.py
@@ -29,5 +29,31 @@ from keras.layers import LSTM
 from keras.layers import Dense
 
 m=Sequential()
-m.add(LSTM(4,activation='sigmoid',))
+m.add(LSTM(4,activation='sigmoid',input_shape=(None,1)))
+m.add(Dense(1))
+m.compile('adam',loss='mean_squared_error')
+
+#fit
+m.fit(x,y,epochs=50)
+
+#predict
+t=pd.read_csv('Google_Stock_Price_Test.csv')
+real_price=t.iloc[:,1:2].values
+test=real_price
+test=sc.transform(test)
+test=np.reshape(test,(20,1,1))
+price=m.predict(test)
+p=sc.inverse_transform(price)
+
+#visulize
+import matplotlib.pyplot as plt
+plt.plot(real_price, color = 'red', label = 'Real Google Stock Price')
+plt.plot(p, color = 'blue', label = 'Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
+
+
 
