a0d81872f1dc83a63e6d15fff9b3fc039a6428a3
Fangling Zhang <luojijiaren@163.com>
2017-10-25 10:33:34 -0400

    20 timesteps and 4 LSTM layers

--- udemy course/RNN/rnn.py
+++ udemy course/RNN/rnn.py
@@ -9,19 +9,23 @@ import pandas as pd
 
 # Importing the training set
 training_set = pd.read_csv('Google_Stock_Price_Train.csv')
-training_set = training_set.iloc[:,1:2].values
+training = training_set.iloc[:,1:2].values
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler()
-training_set = sc.fit_transform(training_set)
+sc = MinMaxScaler(feature_range=(0,1))
+training_set = sc.fit_transform(training)
 
 # Getting the inputs and the ouputs
-X_train = training_set[0:1257]
-y_train = training_set[1:1258]
+X_train=[]
+y_train=[]
+for i in range(20,1258):
+    X_train.append(training_set[i-20:i,0])
+    y_train.append(training_set[i,0])
+X_train,y_train=np.array(X_train),np.array(y_train)
 
 # Reshaping
-X_train = np.reshape(X_train, (1257, 1, 1))  #add a timestep dimension
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))  #add a number of feature 
 
 # Part 2 - Building the RNN
 
@@ -34,32 +38,40 @@ from keras.layers import LSTM
 regressor = Sequential()
 
 # Adding the input layer and the LSTM layer
-regressor.add(LSTM(units = 4, activation = 'sigmoid', input_shape = (None, 1)))
-
+regressor.add(LSTM(units = 3, input_shape = (None, 1),return_sequences=True))
+regressor.add(LSTM(units = 3, return_sequences=True))
+regressor.add(LSTM(units = 3, return_sequences=True))
+regressor.add(LSTM(units = 3))
 # Adding the output layer
 regressor.add(Dense(units = 1))
 
 # Compiling the RNN
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+regressor.compile(optimizer = 'rmsprop', loss = 'mean_squared_error')
 
 # Fitting the RNN to the Training set
-regressor.fit(X_train, y_train, batch_size = 32, epochs = 200)
+regressor.fit(X_train, y_train, batch_size = 32, epochs = 100)
 
 # Part 3 - Making the predictions and visualising the results
 
 # Getting the real stock price of 2017
 test_set = pd.read_csv('Google_Stock_Price_Test.csv')
 real_stock_price = test_set.iloc[:,1:2].values
+r=np.concatenate((training[0:1258],real_stock_price),axis=0)
 
 # Getting the predicted stock price of 2017
-inputs = real_stock_price
-inputs = sc.transform(inputs)
-inputs = np.reshape(inputs, (20, 1, 1))
+stock = r
+stock = sc.fit_transform(stock)
+inputs=[]
+for i in range(1258,1278):
+    inputs.append(stock[i-20:i,0])
+inputs=np.array(inputs)
+
+inputs = np.reshape(inputs, (inputs.shape[0], inputs.shape[1], 1))
 predicted_stock_price = regressor.predict(inputs)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 # Visualising the results
-plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
+plt.plot(r[1258:], color = 'red', label = 'Real Google Stock Price')
 plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
 plt.title('Google Stock Price Prediction')
 plt.xlabel('Time')
